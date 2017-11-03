1e5a0539abbaf3192c9b241178c5c2b76007da88
James McElwain <jmcelwain@gmail.com>
2017-11-03 00:46:41 -0500

    add more channels

--- src/async_sound/core.clj
+++ src/async_sound/core.clj
@@ -25,7 +25,16 @@
 (def audio-format-mono-16
   (let [sample-rate 44100
         sample-size 16
-        channels 1
+        channels 1 
+        signed true
+        big-endian false]
+    (javax.sound.sampled.AudioFormat. sample-rate sample-size channels signed big-endian)))
+
+
+(def audio-format-4-16
+  (let [sample-rate 44100
+        sample-size 16
+        channels 4
         signed true
         big-endian false]
     (javax.sound.sampled.AudioFormat. sample-rate sample-size channels signed big-endian)))
@@ -55,21 +64,54 @@
 
 ;; lib
 
-(defn listener [{name :name audio-format :audio-format min :min max :max chan :chan frame-rate :frame-rate}]
+(defn listener [{name :name
+                 audio-format :audio-format
+                 min :min
+                 max :max
+                 chan-1 :chan-1
+                 chan-2 :chan-2
+                 chan-3 :chan-3
+                 chan-4 :chan-4
+                 frame-rate :frame-rate}]
   (fn []
     (async/thread
       (with-open [line (-> name mixer get-line (open-line audio-format))
                   out (java.io.ByteArrayOutputStream.)]
-        (let [size (/ (.getBufferSize line) 1)
+        (let [size (/ (.getBufferSize line) frame-rate)
               buffer (byte-array size)]
           ;; loop
           (loop []
             (do
               (.reset out)
-              (when-let* [count (did-read line buffer size)
-                          ba (.toByteArray (do (.write out buffer 0 size) out))
-                          frames (partition-all 2 ba)
-                          avg (average (reduce-frames frames))]
-                (println avg)
-                (async/>!! chan avg)
+              (when-let* [count     (did-read line buffer size)
+                          ba        (.toByteArray (do (.write out buffer 0 size) out))
+                          frames    (partition-all 4 (partition-all 2 ba))
+                          {a :a
+                           b :b
+                           c :c
+                           d :d}    (reduce (fn [{a :a b :b c :c d :d} [af bf cf df]]
+                                              {:a (conj a af)
+                                               :b (conj b bf)
+                                               :c (conj c cf)
+                                               :d (conj d df)})
+                                            {:a [] :b [] :c [] :d []} frames)]
+                (println chan-1 chan-2 chan-3 chan-4)
+                (cond
+                  (not (nil? chan-1)) (async/>!! chan-1 (average (reduce-frames a)))
+                  (not (nil? chan-2)) (async/>!! chan-2 (average (reduce-frames b)))
+                  (not (nil? chan-3)) (async/>!! chan-3 (average (reduce-frames c)))
+                  (not (nil? chan-4)) (async/>!! chan-4 (average (reduce-frames d))))
                 (recur)))))))))
+
+
+
+(def chan-1 (async/chan (async/buffer 1)))
+(def chan-2 (async/chan (async/buffer 1)))
+(def chan-3 (async/chan (async/buffer 1)))
+(def chan-4 (async/chan (async/buffer 1)))
+
+(def listener (listener {:chan-1 chan-1 :chan-2 chan-2 :chan-3 chan-3 :chan-4 chan-4 :audio-format audio-format-4-16 :name "ES8" :frame-rate 30}))
+
+(listener)
+
+ (async/<!! chan-2)
