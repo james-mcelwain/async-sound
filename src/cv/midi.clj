b2d5e13a31575f89880e0fe0f0a28f0fcb028650
James McELwain <jmcelwain@gmail.com>
2018-01-02 23:22:26 -0600

    midi

--- src/cv/midi.clj
+++ src/cv/midi.clj
@@ -1,27 +1,29 @@
 (ns cv.midi
-(:require [overtone.midi :as midi]
-          [cv.core :as core]
-          [clojure.core.async :as async]))
+  (:require [overtone.midi :as midi]
+            [cv.core :as core]
+            [clojure.core.async :as async]))
+
+(defn event-handler [channels]
+  (println "listening to " channels)
+  (fn [midi-msg]
+    (println midi-msg)
+    ;; index of channel == cc number
+    (if-let [chan (get channels (:note midi-msg))]
+      (async/>!! chan (:velocity midi-msg)))))
 
 (defn midi-bus [{:keys [channels name]}]
   (println (str "open midi bus " name))
   (if-let [device (midi/midi-in name)]
     (do
       (println (str "openend " name))
-      (midi/midi-handle-events
-       device
-       (fn [midi-msg]
-         (println midi-msg)
-         (if-let [chan (get channels (:note midi-msg))]
-           (async/>!! chan (:velocity midi-msg))))))))
+      (midi/midi-handle-events device (event-handler channels)))))
 
 (def cc0 (core/channel))
 
 (defn debug []
-  (midi-bus {:channels [cc0] :name "iac"})
-  (async/thread
-    (loop []
-      (println (async/<!! cc0))
-      (recur))))
-
-(debug)
+  (let [d (midi-bus {:channels [cc0] :name "iac"})]
+    (async/thread
+      (loop []
+        (println (async/<!! cc0))
+        (recur)))
+    d))
