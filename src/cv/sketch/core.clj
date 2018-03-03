2b77a43aeaeeacef0e1032fd69087b5a67d934ee
James McElwain <jmcelwain@gmail.com>
2018-03-03 14:47:26 -0600

    wrap default sketch

--- src/cv/sketch/core.clj
+++ src/cv/sketch/core.clj
@@ -1,51 +1,50 @@
 (ns cv.sketch.core
   (:require
    [clojure.spec.alpha :as s]
-   [expound.apha :as expound]
+   [expound.alpha :as expound]
    [cv.core :as core]
    [clojure.core.async :as async]
    [quil.core :as q]
    [quil.middleware :as m]))
 
-;; (defn setup []
-;;   (q/frame-rate 30)
-;;   0)
+;; mutable vars
+(def !current-sketch (atom :default))
+(defn default-setup []
+  (q/frame-rate 30)
+  (q/background 0)
+  {})
+(defn default-update [state] state)
+(defn default-draw [state])
+(def default-sketch {:setup default-setup
+                     :update default-update
+                     :draw default-draw})
+(def !sketches (atom {:default default-sketch}))
 
-;; (defn update-state [state]
-;;   (q/map-range (first (async/alts!! [core/c0] :default (:val state)))))
-
-;; (defn draw-state [state]
-;;   (q/stroke-cap :square)
-;;   (q/smooth)
-;;   (q/no-fill)
-;;   (q/background 255)
-;;   (q/translate (/ (q/width) 2) (/ (q/height) 2))
-;;   (q/stroke-weight (q/abs (/ (q/mouse-y) 20)))
-;;   (let [circle-resolution (q/map-range state 0 (q/height) 2 80)
-;;         radius (+ (- state (/ (q/width) 2)) 0.5)
-;;         angle  (/ q/TWO-PI circle-resolution)]
-;;     (q/begin-shape)
-;;     (doseq [i (range circle-resolution)
-;;             :let [x (* (q/cos (* angle i)) radius)
-;;                   y (* (q/sin (* angle i)) radius)]]
-;;       (q/line 0 0 x y))
-;;     (q/end-shape)))
-
-;; (q/defsketch gen-art
-;;   :title "Hello, shape"
-;;   :size [600 600]
-;;   :setup setup
-;;   :update update-state
-;;   :draw draw-state
-;;   :features [:no-bind-output]
-;;   :middleware [m/fun-mode])
-
-
-
-(def sketches (atom {}))
-
+;; ctor
 (defn sketch [key sketch-fns]
-  (expound/expound ::sketch sketch-fns))
+  (expound/expound ::sketch sketch-fns)
+  (swap! !sketches assoc key sketch-fns))
 
 (s/def ::sketch
   (s/keys :req-un [::setup ::update ::draw]))
+
+;; run
+(defn setup-wrapper []
+  (apply (:setup (@!current-sketch @!sketches)) nil))
+(defn update-wrapper [state]
+  (println state)
+  (apply (:update (@!current-sketch @!sketches)) state))
+(defn draw-wrapper [state]
+  (apply (:draw (@!current-sketch @!sketches)) state))
+
+(defn init []
+  (q/defsketch gen-art
+    :title "cv"
+    :size [600 600]
+    :setup setup-wrapper
+    :update update-wrapper
+    :draw draw-wrapper
+    :features [:no-bind-output]
+    :middleware [m/fun-mode]))
+
+(init)
