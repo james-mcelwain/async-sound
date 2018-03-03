3e08ae7bd4b49bef22bedc665474585b363f5c3c
James McElwain <jmcelwain@gmail.com>
2018-03-03 12:40:27 -0600

    version bump

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject cv "0.1.0-SNAPSHOT"
+(defproject cv "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.async "0.3.443"]
                  [overtone/midi-clj "0.5.0"]
                  [quil "2.6.0"]])
