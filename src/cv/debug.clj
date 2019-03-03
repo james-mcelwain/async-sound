0417146565a768d2d2d6e36c1600a95d801e5420
jcm <jmcelwain@gmail.com>
2019-03-02 22:13:10 -0800

    cleanup.

--- src/cv/debug.clj
+++
@@ -1,9 +0,0 @@
-(ns cv.debug
-  (:require [cv.core :refer [c0 c1 c2 c3]]
-            [clojure.core.async :as async]))
-
-(defn debug []
-  (async/thread
-    (loop []
-      (println (clojure.string/join " " (map async/<!! [c0 c1 c2 c3])))
-      (recur))))
--- src/cv/redis.clj
+++
@@ -1,6 +0,0 @@
-(ns cv.redis
-  (:require [taoensso.carmine :as car :refer (wcar)]))
-
-(def con {:pool {} :spec {:host "127.0.0.1" :port 6379}})
-
-(defmacro wcar* [& body] `(car/wcar con ~@body))
