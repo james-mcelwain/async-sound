ff3234543c7245fb6e8b4c54b1275c80dcb5089a
jcm <jmcelwain@gmail.com>
2019-03-02 22:14:49 -0800

    remove midi scripts.

--- scripts/browser-midi.js
+++
@@ -1,5 +0,0 @@
-async function main() {
-	const midi = await navigator.requestMIDIAccess()
-	const hw1 = Array.from(midi.inputs.values()).find(x => x.name === "VirMIDI 1-0")
-	hw1.onmidimessage = console.log.bind(console)
-}
